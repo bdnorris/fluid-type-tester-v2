8a54fee3c7dd010430ba275c2a4a7358b0bf9916
Brian <bdnorris@gmail.com>
2022-12-30 13:19:44 -0600

    multiple articles

--- src/components/Text.vue
+++ src/components/Text.vue
@@ -1,80 +1,26 @@
 <template>
-	<div class="text" :style="`--body-size-min: ${bodySizeMin}px; --body-size-fluid: ${bodySizeFluid}vw; --body-size-max: ${bodySizeMax}px; --header-size-min: ${headerSizeMin}px; --header-size-fluid: ${headerSizeFluid}vw; --header-size-max: ${headerSizeMax}px;`" contenteditable="true">
-		<h1>Down the Rabbit-Hole</h1>
-		<h2 v-if="headingCount > 1">Alice's Adventures in Wonderland</h2>
-		<h3 v-if="headingCount > 2">Alice's Adventures in Wonderland</h3>
-		<h4 v-if="headingCount > 3">Alice's Adventures in Wonderland</h4>
-		<h5 v-if="headingCount > 4">Alice's Adventures in Wonderland</h5>
-		<h6 v-if="headingCount > 5">Alice's Adventures in Wonderland</h6>
-		<p>
-			Alice was beginning to get very tired of sitting by her sister on the
-			bank, and of having nothing to do: once or twice she had peeped into the
-			book her sister was reading, but it had no pictures or conversations in
-			it, “and what is the use of a book,” thought Alice “without pictures or
-			conversations?”
-		</p>
-		<p>
-			So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her.
-		</p>
-		<p>
-			There was nothing so _very_ remarkable in that; nor did Alice think it so
-			_very_ much out of the way to hear the Rabbit say to itself, “Oh dear! Oh
-			dear! I shall be late!” (when she thought it over afterwards, it occurred
-			to her that she ought to have wondered at this, but at the time it all
-			seemed quite natural); but when the Rabbit actually _took a watch out of
-			its waistcoat-pocket_, and looked at it, and then hurried on, Alice
-			started to her feet, for it flashed across her mind that she had never
-			before seen a rabbit with either a waistcoat-pocket, or a watch to take
-			out of it, and burning with curiosity, she ran across the field after it,
-			and fortunately was just in time to see it pop down a large rabbit-hole
-			under the hedge.
-		</p>
-		<p>
-			In another moment down went Alice after it, never once considering how in
-			the world she was to get out again.
-		</p>
-		<p>
-			The rabbit-hole went straight on like a tunnel for some way, and then
-			dipped suddenly down, so suddenly that Alice had not a moment to think
-			about stopping herself before she found herself falling down a very deep
-			well.
-		</p>
-		<p>
-			Either the well was very deep, or she fell very slowly, for she had plenty
-			of time as she went down to look about her and to wonder what was going to
-			happen next. First, she tried to look down and make out what she was
-			coming to, but it was too dark to see anything; then she looked at the
-			sides of the well, and noticed that they were filled with cupboards and
-			book-shelves; here and there she saw maps and pictures hung upon pegs. She
-			took down a jar from one of the shelves as she passed; it was labelled
-			“ORANGE MARMALADE”, but to her great disappointment it was empty: she did
-			not like to drop the jar for fear of killing somebody underneath, so
-			managed to put it into one of the cupboards as she fell past it.
-		</p>
-		<p>
-			“Well!” thought Alice to herself, “after such a fall as this, I shall
-			think nothing of tumbling down stairs! How brave they’ll all think me at
-			home! Why, I wouldn’t say anything about it, even if I fell off the top of
-			the house!” (Which was very likely true.)
-		</p>
-		<p>
-			Down, down, down. Would the fall _never_ come to an end? “I wonder how
-			many miles I’ve fallen by this time?” she said aloud. “I must be getting
-			somewhere near the centre of the earth. Let me see: that would be four
-			thousand miles down, I think—” (for, you see, Alice had learnt several
-			things of this sort in her lessons in the schoolroom, and though this was
-			not a _very_ good opportunity for showing off her knowledge, as there was
-			no one to listen to her, still it was good practice to say it over) “—yes,
-			that’s about the right distance—but then I wonder what Latitude or
-			Longitude I’ve got to?” (Alice had no idea what Latitude was, or Longitude
-			either, but thought they were nice grand words to say.)
-		</p>
+	<div>
+		<select name="articles" id="articles" v-model="selectedArticle">
+			<option v-for="(article, index) in articles" :value="article.key" :key="index">{{ article.key }}</option>
+		</select>
+		<div class="text" :style="`--body-size-min: ${bodySizeMin}px; --body-size-fluid: ${bodySizeFluid}vw; --body-size-max: ${bodySizeMax}px; --header-size-min: ${headerSizeMin}px; --header-size-fluid: ${headerSizeFluid}vw; --header-size-max: ${headerSizeMax}px;`" contenteditable="true">
+			<h1>{{ article.mainHeading }}</h1>
+			<h2 v-if="headingCount > 1">Lorem ipsum dolor sit amet</h2>
+			<h3 v-if="headingCount > 2">Consectetur adipiscing elit, sed do</h3>
+			<h4 v-if="headingCount > 3">Eiusmod tempor incididunt ut</h4>
+			<h5 v-if="headingCount > 4">Labore et dolore magna aliqua</h5>
+			<h6 v-if="headingCount > 5">Leo vel fringilla est ullamcorper eget</h6>
+			<p v-for="(paragraph, index) in article.paragraphs" :key="index">
+				{{ paragraph }}
+			</p>
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
 import { useStore } from "../../store";
 import { computed, ref, watch } from "vue";
+import texts from "../../texts.json";
 
 export default {
 	setup() {
@@ -86,6 +32,14 @@ export default {
 		const headerSizeFluid = computed(() => store.state.headerSizeFluid);
 		const headerSizeMax = computed(() => store.state.headerSizeMax);
 		const headingCount = computed(() => store.state.headingLevels);
+		const articles = JSON.parse(JSON.stringify(texts)).texts;
+		// console.log('articles', articles)
+		const selectedArticle = ref(articles[0].key);
+		const article = ref(articles[0]);
+		watch(selectedArticle, (newVal) => {
+			// console.log('newVal', newVal)
+			article.value = articles.find((article) => article.key === newVal);
+		});
 		return {
 			bodySizeMin,
 			bodySizeFluid,
@@ -94,6 +48,9 @@ export default {
 			headerSizeFluid,
 			headerSizeMax,
 			headingCount,
+			article,
+			articles,
+			selectedArticle
 		};
 	},
 };
