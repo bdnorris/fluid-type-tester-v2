ee6fbe12fbaf0aa84144a082a81745bb42097a44
Brian <bdnorris@gmail.com>
2023-01-02 19:45:01 -0600

    edit content enable

--- src/components/Text.vue
+++ src/components/Text.vue
@@ -9,10 +9,12 @@
 				{{ article.key }}
 			</option>
 		</select>
+		<input type="checkbox" value="edit-content" id="edit-content" v-model="contentEditable" />
+		<label for="edit-content">Edit Content</label>
 		<div
 			class="text"
 			:style="`--body-size-min: ${bodySizeMin}px; --body-size-fluid: ${bodySizeFluid}vw; --body-size-max: ${bodySizeMax}px; --body-line-height: ${bodyLineHeight}; --header-size-min: ${headerSizeMin}px; --header-size-fluid: ${headerSizeFluid}vw; --header-size-max: ${headerSizeMax}px; --header-ratio: ${headerRatio}; --h2-size-min: ${h2Size[0]}px; --h2-size-fluid: ${h2Size[1]}vw; --h2-size-max: ${h2Size[2]}px; --h3-size-min: ${h3Size[0]}px; --h3-size-fluid: ${h3Size[1]}vw; --h3-size-max: ${h3Size[2]}px; --h4-size-min: ${h4Size[0]}px; --h4-size-fluid: ${h4Size[1]}vw; --h4-size-max: ${h4Size[2]}px; --h5-size-min: ${h5Size[0]}px; --h5-size-fluid: ${h5Size[1]}vw; --h5-size-max: ${h5Size[2]}px; --h6-size-min: ${h6Size[0]}px; --h6-size-fluid: ${h6Size[1]}vw; --h6-size-max: ${h6Size[2]}px; --header-line-height: ${headerLineHeight}; --body-font: ${bodyFont}; --header-font: ${headerFont}`"
-			contenteditable="true"
+			:contenteditable="contentEditable"
 		>
 			<h1>{{ article.mainHeading }}</h1>
 			<h2 v-if="headingCount > 1">Lorem ipsum dolor sit amet</h2>
@@ -20,9 +22,7 @@
 			<h4 v-if="headingCount > 3">Eiusmod tempor incididunt ut</h4>
 			<h5 v-if="headingCount > 4">Labore et dolore magna aliqua</h5>
 			<h6 v-if="headingCount > 5">Leo vel fringilla est ullamcorper eget</h6>
-			<p v-for="(paragraph, index) in article.paragraphs" :key="index">
-				{{ paragraph }}
-			</p>
+			<p v-for="(paragraph, index) in article.paragraphs" :key="index" v-html="paragraph" />
 		</div>
 	</div>
 </template>
@@ -111,6 +111,7 @@ export default {
 			h5Size,
 			h6Size,
 			headerLineHeight,
+			contentEditable: ref(false),
 		};
 	},
 };
